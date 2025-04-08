183b8bc8531a5cd7a54adc51ef0fbd4a01f04d61
Souheyb-6J <souheyb.mafi@inraci.be>
2025-04-08 08:43:49 +0000

    Change

--- Souheyb.mafi.py
+++ Souheyb.mafi.py
@@ -1,12 +1,3 @@
-print("ok")
-jeux = input("ok : ")
-print(jeux)
-print(jeux)
-ok = input("ecole")
-ecout = ok
-travai = ("ok")
-
-
-
-lqjkshdlksjhsldkjqshkjdqshjkdshjkqsdskjsqhdkjksqhdkjsq
-wxcqsds
+# projet
+#
+#
